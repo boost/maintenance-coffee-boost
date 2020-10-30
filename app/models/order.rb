cdd6c9c58ce5b39788d76d119bc4aade7c9c61e4
katherine-boost <katherine@boost.co.nz>
2020-10-30 13:04:24 +1300

    spec(Order model): add spec, and remove uniqueness constraint on name
    We decided that there's really no need for it to be unique, in a real-world situation that would probably be annoying

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
   has_many :order_items
 
   accepts_nested_attributes_for :order_items, allow_destroy: true
 
   validates :name, presence: true
-  validates :name, uniqueness: true, if: ->(o) { o.name.present? }
 end
